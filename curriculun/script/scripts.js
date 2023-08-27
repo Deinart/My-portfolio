cc4916e90f13c8058cd82c034a2483c0533e0b90
daniel martinez <116817836+Deinart@users.noreply.github.com>
2023-08-27 19:27:33 -0400

    Delete curriculun directory

--- curriculun/script/scripts.js
+++
@@ -1,7 +0,0 @@
-const toggleActiveMenu = document.getElementById("box-img");
-
-toggleActiveMenu.addEventListener('click', () => {
-    document.getElementById('box-menu-mobile').classList.toggle('active');
-    document.getElementById('toggle-menu').classList.toggle('active');
-  
-});
