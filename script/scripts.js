978534f4cbd8fac863cb43bca2ca7d325be6b937
Daniel Martinez <danielitobenitez10@gmail.com>
2023-08-29 10:50:57 -0400

    Añadiendo las pantallas al CSS

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,7 @@
-const toggleActive = document.getElementById("box-img");
+const toggleMenu = document.getElementById("box-img");
 
-
-toggleActive.addEventListener('click', () => {
-    document.getElementById('box-menu-mobile').classList.toggle('active');
+toggleMenu.addEventListener('click', () => {
+    document.getElementById('box-menu-tablet').classList.toggle('active');
     document.getElementById('toggle-menu').classList.toggle('active');
     document.getElementById('menu-logo').classList.toggle('active');
     document.getElementById('box-ul').classList.toggle('active');
