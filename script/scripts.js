7de7fc86f3871ca9cc1021d85d46089c0b0ece9d
Daniel Martinez <danielitobenitez10@gmail.com>
2023-08-31 12:13:19 -0400

    Cambiando la barra de mobile a table y creando la nueva de mobile

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,8 @@
 const toggleMenu = document.getElementById("box-img");
 
 toggleMenu.addEventListener('click', () => {
-    document.getElementById('box-menu-tablet').classList.toggle('active');
+    document.getElementById('bar-menu-tablet').classList.toggle('active');
     document.getElementById('toggle-menu').classList.toggle('active');
     document.getElementById('menu-logo').classList.toggle('active');
-    document.getElementById('box-ul').classList.toggle('active');
+    document.getElementById('box-ul-tablet').classList.toggle('active');
 });
